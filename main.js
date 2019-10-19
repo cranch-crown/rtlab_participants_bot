ab49ac8e7363dd508d487b5ef461e6c31b324eb2
guldra_cranch <ymzlgcranch@gmail.com>
2019-10-19 15:16:06 +0900

    mainから責務を分割した

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //connpass API で現在の参加者数を取得
-function fetchParticipants(event_id) {
+function fetchParticipants() {
+  //イベント毎にスクリプトプロパティにIDを設定する！！
+  const EVENT_ID = PropertiesService.getScriptProperties().getProperty("EVENT_ID");
+
   const CONNPASS_API_ENDPOINT = "https://connpass.com/api/v1/event/";
 
-  var url = CONNPASS_API_ENDPOINT + '?event_id=' + event_id;
+  var url = CONNPASS_API_ENDPOINT + '?event_id=' + EVENT_ID;
   var options = {
     'method': 'GET'
   };
@@ -12,22 +15,21 @@ function fetchParticipants(event_id) {
   return response.events[0].accepted;
 }
 
-function main() {
-  //イベント毎にスクリプトプロパティにIDを設定する！！
-  const EVENT_ID = PropertiesService.getScriptProperties().getProperty("EVENT_ID");
-
+function fetchParticipantsFluctuation() {
   //1分前の参加者数をキャッシュから取得
   var cache = CacheService.getScriptCache();
-  var patricipants_one_minutes_ago = cache.get('participants_one_mitites_ago');
+  var partricipants_one_minutes_ago = cache.get('participants_one_mitites_ago');
 
   //今回取得した参加者でキャッシュを上書き
-  var participants = fetchParticipants(EVENT_ID);
+  var participants = fetchParticipants();
   cache.put('participants_one_mitites_ago', participants, 10);
   
-  Logger.log(participants);
-  if (participants == patricipants_one_minutes_ago) {
+  if(!partricipants_one_minutes_ago) {
     return 0;
   }
-  
-  
+  return participants - partricipants_one_minutes_ago
+}
+
+function main() {
+  fetchParticipants()
 }
